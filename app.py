b0baaf985abb06415f5183b6e9b402ee759c55a2
Rianna <rianna.cleary@gmail.com>
2020-07-23 13:19:46 -0400

    mad libs

--- app.py
+++ app.py
@@ -2,8 +2,17 @@
 
 # MINI CALCULATOR
 #
-num1 = input("Enter a number: ")
-num2 = input("*DJ Khalid voice* Anotha one: ")
-result = float(num1) + float(num2)
+# num1 = input("Enter a number: ")
+# num2 = input("*DJ Khalid voice* Anotha one: ")
+# result = float(num1) + float(num2)
+#
+# print(result)
 
-print(result)
+# MAD LIBS
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a plural noun: ")
+# celebrity = input("Enter a celebrity: ")
+#
+# print("Roses are " + color)
+# print(plural_noun + " are blue")
+# print("I love " + celebrity)
