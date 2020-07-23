3d25dffccc1dea4e15308b35924d5392d07b1485
Rianna <rianna.cleary@gmail.com>
2020-07-23 13:08:43 -0400

    added calulator

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# print("Hello World")
+
+# MINI CALCULATOR
+#
+num1 = input("Enter a number: ")
+num2 = input("*DJ Khalid voice* Anotha one: ")
+result = float(num1) + float(num2)
+
+print(result)
