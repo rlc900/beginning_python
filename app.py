2fecbf0ab03d4417ab7ada75e6a6b47b5ed97ed9
Rianna <rianna.cleary@gmail.com>
2020-07-28 12:18:20 -0400

    guessing game

--- app.py
+++ app.py
@@ -58,5 +58,23 @@ numbers  =[1,4,67,8,24,53,10]
 # else:
 #     print("Invalid operator")
 
+secret_word = "slinky"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input('Enter guess: ')
+        guess_count += 1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+    print("Out of Guesses, YOU LOSE!")
+else:
+    print("You win!")
+
+
 
 
